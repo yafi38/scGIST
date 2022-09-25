d47a226a9e47e2debc356b08e4373b4ffa46dde2
Mashrur Ahmed Yafi <yafi2475@gmail.com>
2022-09-25 22:15:31 +0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='scGIST',
+    version='1.0',
+    packages=['scGIST'],
+    url='https://github.com/yafi38/scGIST',
+    license='',
+    author='yafi38',
+    author_email='yafi2475@gmail.com',
+    description=''
+)
