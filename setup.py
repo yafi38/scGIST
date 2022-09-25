f4b3a6b1f51b8ba4c8db801d36658c36758da312
Mashrur Ahmed Yafi <yafi2475@gmail.com>
2022-09-25 22:21:39 +0600

    Add requirements

--- setup.py
+++ setup.py
@@ -8,5 +8,14 @@ setup(
     license='',
     author='yafi38',
     author_email='yafi2475@gmail.com',
-    description=''
+    description='',
+    install_requires=[
+        'sklearn~=0.0',
+        'scikit-learn~=1.0.1',
+        'tensorflow~=2.5.0',
+        'numpy~=1.19.5',
+        'matplotlib~=3.5.0',
+        'seaborn~=0.11.2',
+        'pandas~=1.3.4'
+    ]
 )
